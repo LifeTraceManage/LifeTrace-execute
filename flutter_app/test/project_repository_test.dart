7dd39ac3f17d687ac6ddf2b0f9686f30fb8f8ad3
zhouxingxing1279 <76243394+zhouxingxing1279@users.noreply.github.com>
2026-09-11 11:52:51 +0800

    test: remove redundant drift import

--- flutter_app/test/project_repository_test.dart
+++ flutter_app/test/project_repository_test.dart
@@ -1,4 +1,3 @@
-import 'package:drift/drift.dart' hide isNull;
 import 'package:drift/native.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lifetrace_execute/data/local/app_database.dart';
