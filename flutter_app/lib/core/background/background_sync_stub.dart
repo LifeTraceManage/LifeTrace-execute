38ef2da958144856f5ccd9a4d2f90367a2e2fbb0
zhouxingxing1279 <76243394+zhouxingxing1279@users.noreply.github.com>
2026-09-11 17:26:23 +0800

    feat: mirror background logout no-op on web

--- flutter_app/lib/core/background/background_sync_stub.dart
+++ flutter_app/lib/core/background/background_sync_stub.dart
@@ -5,4 +5,5 @@ class BackgroundSyncScheduler {
   static Future<void> schedulePeriodic() async {}
   static Future<void> enqueueInitialSync() async {}
   static Future<void> enqueueAfterLocalChange() async {}
+  static Future<void> cancelForLogout() async {}
 }
