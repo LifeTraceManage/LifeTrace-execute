320dc2b050754c1c6c3f26cd7a40c8f196b348f4
zhouxingxing1279 <76243394+zhouxingxing1279@users.noreply.github.com>
2026-09-16 14:09:59 +0800

    feat: wire About page

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -35,6 +35,7 @@ import 'features/goals/goal_providers.dart';
 import 'features/habits/habit_providers.dart';
 import 'features/important_dates/important_date_providers.dart';
 import 'features/projects/project_providers.dart';
+import 'features/profile/about_providers.dart';
 import 'features/profile/profile_providers.dart';
 import 'features/profile/sync_health_providers.dart';
 import 'features/reminders/reminder_providers.dart';
@@ -56,6 +57,7 @@ part 'screens_profile_devices.dart';
 part 'screens_profile_data.dart';
 part 'screens_profile_security.dart';
 part 'screens_sync_health.dart';
+part 'screens_profile_about.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
